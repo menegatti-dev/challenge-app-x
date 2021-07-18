af3d29d3adf563cedf04c67c80f5cd2c43140e7d
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-18 03:01:04 -0300

    feat: add styled-components theme

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>App X</Title>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    background: '#181829',
+    black: '#222232',
+    blue: '#246BFD',
+    red: '#ff0033',
+    white: '#ffffff',
+    gray: '#C4C4C4',
+    darkGray: '#65656B',
+  },
+};
