a08d9417f5ee3a211d790f6d50f3440c20275d90
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-18 12:54:08 -0300

    chore: add react-native-feather@1.1.2

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { ArrowLeft } from 'react-native-feather';
 import { Container, Title } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Title>App X</Title>
+      <ArrowLeft color="#f00" width={20} height={20} />
     </Container>
   );
 };
