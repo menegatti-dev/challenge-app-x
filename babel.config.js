12f94969000960d9161d1079d9c3805c1265d8cf
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-18 02:27:36 -0300

    feat: add absolute import with babel plugin module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports =  function(api) {
+  api.cache(true)
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            "@": "./src",
+          }
+        }
+      ]
+    ]
+  }
 };
