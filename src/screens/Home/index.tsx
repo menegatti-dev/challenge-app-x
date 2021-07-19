c176116d83c65a97daccba83497db7f476f71266
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-19 00:02:31 -0300

    feat: add Home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
-import { ArrowLeft } from 'react-native-feather';
-import { Container, Title } from './styles';
+import { League } from '@/types/league';
+import { LeagueCard } from '@/components/LeagueCard';
+import { Search } from '@/components/Search';
+import { EmptyLeague } from '@/components/EmptyLeague';
+import { routeNames } from '@/routes/routeNames';
+import leaguesJson from '@/mocks/leagues.json';
+import seasonsJson from '@/mocks/seasons.json';
+
+import { Container, Title, SeasonsList, SeasonsCard, SeasonsCardText, LeagueList, ListSeparator } from './styles';
 
 const Home: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  const [leagues, setLeagues] = useState<League[]>([]);
+  const [seasons, setSeasons] = useState<number[]>([]);
+  const [selectedSeasons, setSelectedSeasons] = useState<number>(new Date().getFullYear());
+  const [searchText, setSearchText] = useState('');
+
+  const leaguesFiltered = useMemo(
+    () => [
+      ...leagues.filter(
+        item => item.league.name.includes(searchText) && item.seasons?.find(value => value.year === selectedSeasons),
+      ),
+    ],
+    [leagues, searchText, selectedSeasons],
+  );
+
+  const renderItem = useCallback(
+    ({ item }: { item: League }) => (
+      <LeagueCard
+        league={item.league.name}
+        logo={item.league.logo}
+        country={item.country.name}
+        onPress={() => navigate(routeNames.CLASSIFICATION, { season: selectedSeasons, league: item.league.id })}
+      />
+    ),
+    [navigate, selectedSeasons],
+  );
+
+  const seasonRenderItem = useCallback(
+    ({ item }: { item: number }) => (
+      <SeasonsCard active={item === selectedSeasons} onPress={() => setSelectedSeasons(item)}>
+        <SeasonsCardText>{item}</SeasonsCardText>
+      </SeasonsCard>
+    ),
+    [selectedSeasons],
+  );
+
+  const leaguesHeaderComponent = useCallback(
+    () => (
+      <>
+        <Title>Selecione a temporada</Title>
+        <SeasonsList
+          data={seasons.sort((a, b) => a + b)}
+          horizontal
+          ItemSeparatorComponent={ListSeparator}
+          keyExtractor={item => `${item}`}
+          renderItem={seasonRenderItem}
+        />
+        <Title>Selecione a liga</Title>
+      </>
+    ),
+    [seasonRenderItem, seasons],
+  );
+
+  const loadData = () => {
+    setLeagues([...leaguesJson.response]);
+    setSeasons([...seasonsJson.response.sort((a: number, b: number) => b - a)]);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <Container>
-      <Title>App X</Title>
-      <ArrowLeft color="#f00" width={20} height={20} />
+      <Search value={searchText} onChangeText={value => setSearchText(value)} placeholder="Procure sua liga..." />
+
+      <LeagueList
+        data={leaguesFiltered}
+        ListHeaderComponent={leaguesHeaderComponent}
+        ListEmptyComponent={EmptyLeague}
+        keyExtractor={item => `${item.league.id}`}
+        ItemSeparatorComponent={ListSeparator}
+        renderItem={renderItem}
+      />
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,12 +1,52 @@
+import { League } from '@/types/league';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  padding: 16px;
 `;
 
 export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.white};
-  font-size: 18px;
+  font-size: 16px;
+  margin-top: 16px;
+  font-family: 'Poppins-Medium';
+`;
+
+export const SeasonsList = styled(FlatList as new () => FlatList<number>)`
+  width: 100%;
+  padding: 0 0 16px;
+`;
+
+interface SeasonsCardProps {
+  active: boolean;
+}
+
+export const SeasonsCard = styled(RectButton)<SeasonsCardProps>`
+  background: ${({ theme: { colors }, active }) => (active ? colors.blue : colors.black)};
+  border-radius: 5px;
+  height: 36px;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 16px;
+`;
+
+export const SeasonsCardText = styled.Text`
+  font-family: 'Poppins-Regular';
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 16px;
+`;
+
+export const LeagueList = styled(FlatList as new () => FlatList<League>)`
+  flex: 1;
+  padding: 10px 0;
+  width: 100%;
+  margin-top: 10px;
+`;
+
+export const ListSeparator = styled.View`
+  height: 16px;
+  width: 16px;
 `;
