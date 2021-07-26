1363e7bc417af65e29eb1e2352608cded9c38ec9
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-25 22:15:19 -0300

    feat: add Classification page

--- src/screens/Classification/index.tsx
+++ src/screens/Classification/index.tsx
@@ -1,22 +1,166 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import React from 'react';
 
-import { Container, Title } from './styles';
+import { Search } from '@/components/Search';
+import { EmptyLeague } from '@/components/EmptyLeague';
+import { routeNames } from '@/routes/routeNames';
+import leagueJson from '@/mocks/league.json';
+
+import Header from '@/components/Header';
+import { LeagueDetails, StandingsEntity } from '@/types/leagueDetails';
+import TableItem from '@/components/TableItem';
+import {
+  Container,
+  LeagueWrapper,
+  LeagueLogoBackground,
+  LeagueLogo,
+  Title,
+  FilterList,
+  FilterCard,
+  FilterCardText,
+  LeagueList,
+  ListSeparator,
+  LeagueTitle,
+} from './styles';
 
 interface RouteParamsProps {
   season: string;
-  league: string;
+  leagueId: string;
 }
 
 const Classification: React.FC = () => {
-  const navigation = useNavigation();
+  const { navigate } = useNavigation();
   const route = useRoute();
 
-  const { season, league } = route.params as RouteParamsProps;
+  const { season, leagueId } = route.params as RouteParamsProps;
+
+  const [league, setLeague] = useState<LeagueDetails>({} as LeagueDetails);
+
+  const filters = useMemo(
+    () => [
+      { id: 'classification', name: 'Rank' },
+      { id: 'name', name: 'Nome' },
+      { id: 'wins', name: 'Vitorias' },
+      { id: 'draw', name: 'Empates' },
+      { id: 'losers', name: 'Derrotas' },
+      { id: 'golsDiff', name: 'Diferença em gols' },
+    ],
+    [],
+  );
+  const [selectedFilter, setSelectedFilter] = useState<string>('classification');
+  const [searchText, setSearchText] = useState('');
+
+  const teamsFiltered = useMemo(() => {
+    const filtered = league?.standings?.filter(item => item.team.name.includes(searchText)) || [];
+    switch (selectedFilter) {
+      case 'classification':
+        return filtered.sort((a, b) => a.rank - b.rank);
+
+      case 'name':
+        return filtered.sort((a, b) => (b.team.name < a.team.name ? 1 : b.team.name > a.team.name ? -1 : 0));
+
+      case 'wins':
+        return filtered.sort((a, b) => b.all.win - a.all.win);
+
+      case 'draw':
+        return filtered.sort((a, b) => b.all.draw - a.all.draw);
+
+      case 'losers':
+        return filtered.sort((a, b) => b.all.lose - a.all.lose);
+
+      case 'golsDiff':
+        return filtered.sort((a, b) => b.goalsDiff - a.goalsDiff);
+
+      default:
+        return filtered;
+    }
+  }, [league?.standings, searchText, selectedFilter]);
+
+  const renderItem = useCallback(
+    ({ item }: { item: StandingsEntity }) => (
+      <TableItem
+        type="line"
+        logo={item.team.logo}
+        classification={item.rank}
+        name={item.team.name}
+        points={item.points}
+        wins={item.all.win}
+        draw={item.all.draw}
+        losers={item.all.lose}
+        golsDiff={item.goalsDiff}
+        onPress={() => navigate(routeNames.TEAMDETAILS, { teamId: item.team.id })}
+      />
+    ),
+    [navigate],
+  );
+
+  const filterRenderItem = useCallback(
+    ({ item }: { item: { id: string; name: string }; index: number }) => (
+      <FilterCard
+        active={selectedFilter === item.id}
+        onPress={() => {
+          setSelectedFilter(item.id);
+        }}
+      >
+        <FilterCardText>{item.name}</FilterCardText>
+      </FilterCard>
+    ),
+    [selectedFilter],
+  );
+
+  const leaguesHeaderComponent = useCallback(
+    () => (
+      <>
+        <Title>Classificar por</Title>
+        <FilterList
+          data={filters}
+          horizontal
+          ItemSeparatorComponent={ListSeparator}
+          keyExtractor={item => `${item.id}`}
+          renderItem={filterRenderItem}
+        />
+        <TableItem
+          type="header"
+          classification=""
+          name="Nome"
+          points="Pts"
+          wins="V"
+          draw="E"
+          losers="D"
+          golsDiff="DG"
+        />
+      </>
+    ),
+    [filterRenderItem, filters],
+  );
+
+  const loadData = () => {
+    setLeague({ ...leagueJson.response[0].league });
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <Container>
-      <Title>Classification</Title>
+      <Header countryName={league?.country} countryLogo={league?.flag} />
+      <LeagueWrapper>
+        <LeagueLogoBackground>
+          <LeagueLogo source={{ uri: league?.logo }} />
+        </LeagueLogoBackground>
+        <LeagueTitle>{`${league?.name} ${league?.season}`}</LeagueTitle>
+      </LeagueWrapper>
+      <Search value={searchText} onChangeText={value => setSearchText(value)} placeholder="Procure sua liga..." />
+
+      <LeagueList
+        data={teamsFiltered}
+        ListHeaderComponent={leaguesHeaderComponent}
+        ListEmptyComponent={EmptyLeague}
+        keyExtractor={item => `${item.team.id}`}
+        ItemSeparatorComponent={ListSeparator}
+        renderItem={renderItem}
+      />
     </Container>
   );
 };
--- src/screens/Classification/styles.ts
+++ src/screens/Classification/styles.ts
@@ -1,5 +1,77 @@
+import { League } from '@/types/league';
+import { StandingsEntity } from '@/types/leagueDetails';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 16px;
+  align-items: center;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 16px;
+  margin-top: 16px;
+  font-family: 'Poppins-Medium';
+`;
+
+export const LeagueWrapper = styled.View`
+  margin: 24px 0 8px;
+  align-items: center;
+`;
+
+export const LeagueLogoBackground = styled.View`
+  padding: 10px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.colors.black};
+  margin-bottom: 8px;
+`;
+
+export const LeagueLogo = styled.Image`
+  width: 121px;
+  height: 121px;
+`;
+
+export const LeagueTitle = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 16px;
+`;
+
+export const FilterList = styled(FlatList as new () => FlatList<{ id: string; name: string }>)`
+  width: 100%;
+  padding: 0 0 16px;
+`;
+
+interface FilterCardProps {
+  active: boolean;
+}
+
+export const FilterCard = styled(RectButton)<FilterCardProps>`
+  background: ${({ theme: { colors }, active }) => (active ? colors.blue : colors.black)};
+  border-radius: 5px;
+  height: 36px;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 16px;
+`;
+
+export const FilterCardText = styled.Text`
+  font-family: 'Poppins-Regular';
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 16px;
+`;
+
+export const LeagueList = styled(FlatList as new () => FlatList<StandingsEntity>)`
+  flex: 1;
+  padding: 10px 0;
+  width: 100%;
+  margin-top: 10px;
+`;
+
+export const ListSeparator = styled.View`
+  height: 8px;
+  width: 8px;
+`;
