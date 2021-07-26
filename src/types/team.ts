aafa8167ecbc8445914ad2d1427b7d2c29d0b0ac
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-25 22:15:51 -0300

    feat: add Team type

---
+++ src/types/team.ts
@@ -0,0 +1,19 @@
+export interface Team {
+  team: {
+    id: number;
+    name: string;
+    country: string;
+    founded: number;
+    national: boolean;
+    logo: string;
+  };
+  venue: {
+    id: 556;
+    name: string;
+    address: string;
+    city: string;
+    capacity: number;
+    surface: string;
+    image: string;
+  };
+}
