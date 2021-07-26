b13b7649917998cd63b8943d965ba951673f8b25
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-25 22:16:04 -0300

    feat: add Team Details page

--- src/screens/TeamDetails/index.tsx
+++ src/screens/TeamDetails/index.tsx
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRoute } from '@react-navigation/native';
 
-import { Container, Title } from './styles';
+import { Team } from '@/types/team';
+
+import teamJson from '@/mocks/team.json';
+
+import Header from '@/components/Header';
+import {
+  Container,
+  TeamWrapper,
+  TeamLogoBackground,
+  TeamLogo,
+  Row,
+  Column,
+  Title,
+  Value,
+  TeamTitle,
+  SectionTitle,
+  Line,
+  VenueImage,
+} from './styles';
+
+interface RouteParamsProps {
+  teamId: string;
+}
 
 const TeamDetails: React.FC = () => {
+  const route = useRoute();
+
+  const { teamId } = route.params as RouteParamsProps;
+
+  const [data, setData] = useState<Team>({} as Team);
+
+  const loadData = () => {
+    setData({ ...teamJson.response[0] });
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <Container>
-      <Title>Team Details</Title>
+      <Header />
+      <TeamWrapper>
+        <TeamLogoBackground>
+          <TeamLogo source={{ uri: data?.team?.logo }} />
+        </TeamLogoBackground>
+        <TeamTitle>{`${data?.team?.name}`}</TeamTitle>
+      </TeamWrapper>
+      <Row>
+        <Column>
+          <Title>País</Title>
+          <Value>{data?.team?.country}</Value>
+        </Column>
+        <Column>
+          <Title>Fundação</Title>
+          <Value>{data?.team?.founded}</Value>
+        </Column>
+      </Row>
+      <Row>
+        <Line />
+        <SectionTitle>Estádio</SectionTitle>
+        <Line />
+      </Row>
+      <VenueImage resizeMode="cover" source={{ uri: data?.venue?.image }} />
+      <Row>
+        <Column>
+          <Title>Nome</Title>
+          <Value>{data?.venue?.name}</Value>
+        </Column>
+        <Column>
+          <Title>Capacidade</Title>
+          <Value>{data?.venue?.capacity}</Value>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <Title>Endereço</Title>
+          <Value>{data?.venue?.address}</Value>
+        </Column>
+        <Column>
+          <Title>Cidade</Title>
+          <Value>{data?.venue?.city}</Value>
+        </Column>
+      </Row>
     </Container>
   );
 };
--- src/screens/TeamDetails/styles.ts
+++ src/screens/TeamDetails/styles.ts
@@ -1,5 +1,73 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 16px;
+`;
 
-export const Title = styled.Text``;
+export const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 16px;
+`;
+
+export const Column = styled.View`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.gray};
+  font-size: 12px;
+  font-family: 'Poppins-regular';
+`;
+
+export const Value = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 14px;
+  font-family: 'Poppins-regular';
+`;
+
+export const TeamWrapper = styled.View`
+  margin: 24px 0 8px;
+  align-items: center;
+`;
+
+export const TeamLogoBackground = styled.View`
+  padding: 10px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.colors.black};
+  margin-bottom: 8px;
+`;
+
+export const TeamLogo = styled.Image`
+  width: 121px;
+  height: 121px;
+`;
+
+export const TeamTitle = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 16px;
+`;
+
+export const Line = styled.View`
+  background: ${({ theme }) => theme.colors.black};
+  height: 1px;
+  flex: 1;
+`;
+
+export const SectionTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 16px;
+  font-family: 'Poppins-SemiBold';
+  padding: 0px 8px;
+`;
+
+export const VenueImage = styled.Image`
+  width: 100%;
+  height: ${(Dimensions.get('window').width / 16) * 9}px;
+  border-radius: 20px;
+  margin-bottom: 16px;
+`;
