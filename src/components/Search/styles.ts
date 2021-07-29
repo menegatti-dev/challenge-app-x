aa1f4743f9759afba9565a6fcfc5a70a6384c166
Marlon Menegatti <marlon.menegatti@codeby.com.br>
2021-07-29 02:50:26 -0300

    fix: align search icon

--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
   background: ${({ theme }) => theme.colors.black};
   flex-direction: row;
   width: 100%;
-  padding: 16px;
+  padding: ${Platform.OS === 'android' ? 8 : 16}px 16px;
+  align-items: center;
   border-radius: 10px;
 `;
 
